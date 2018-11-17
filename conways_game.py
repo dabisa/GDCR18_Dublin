14217d7edf2b600a3bb22768831450dbb704b4fe
Dabisa Kelava <dabisa.kelava@gmail.com>
2018-11-17 11:57:04 +0000

    deleted for next session

--- conways_game.py
+++ conways_game.py
@@ -1,17 +1,3 @@
 '''
 Code
 '''
-
-class CellState(object):
-    def is_alive(self):
-        return self._is_alive
-
-
-class Alive(CellState):
-    def __init__(self):
-        self._is_alive = True
-
-
-class Dead(CellState):
-    def __init__(self):
-        self._is_alive = False
--- test_conways_game.py
+++ test_conways_game.py
@@ -1,14 +1,3 @@
 '''
 Test
 '''
-
-from conways_game import *
-
-
-def test_is_alive():
-    state = Alive()
-    assert state.is_alive() == True
-
-def test_is_dead():
-    state = Dead()
-    assert state.is_alive() == False
